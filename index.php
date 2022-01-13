c7abca2caf0ea3035d68f1db73fe5f3d6654ba6e
zile028 <zile028@gmail.com>
2022-01-13 08:43:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Driving School</title>
+    <!-- Font Awesome 5 | visit: https://fontawesome.com/v5.15/icons?d=gallery&p=2 -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <!-------- TOP BAR -------->
+
+    <section class="top-bar">
+        <article class="container">
+            <ul>
+                <li><i class="fas fa-phone"></i>1-800-123-6889</li>
+                <li><i class="fas fa-map-marker-alt"></i>8th floor, 379 Hudson St, New York</li>
+                <li><i class="fas fa-clock"></i>09:30 am – 06:00 pm</li>
+            </ul>
+        </article>
+    </section>
+
+    <!-------- NAV -------->
+
+    <nav class="container">
+        <div class="logo">
+            <a href="index.php"><img src="img/logo.png" alt="logo"></a>
+        </div>
+        <div class="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Courses</a></li>
+                <li><a href="index.php">Teachers</a></li>
+                <li><a href="index.php">Blog</a></li>
+                <li><a href="index.php">About</a></li>
+                <li><a href="index.php">Contact</a></li>
+                <li><a href="http://wwww.facebook.com"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="http://wwww.twitter.com"><i class="fab fa-twitter"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+
+</html>
